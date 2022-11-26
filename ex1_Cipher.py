0e40c197e8fb05e37f0fbe213442e77f668ccb8e
Preeti Singh <115339754+ghpreeti@users.noreply.github.com>
2022-11-26 14:48:35 +0530

    Add files via upload

---
+++ ex1_Cipher.py
@@ -0,0 +1,10 @@
+# **********print the following************
+# this is // double backslash
+# these are /\ /\ /\ /\ mountains
+# he is     awesome (use escape sequence instesd of manual spaces)
+# \" \n \t \' (print this as an output)
+
+print("this is \\\\ double backslash")
+print("these are /\\/\\/\\/\\/\\ mountains")
+print("\\\" \\n \\t \\\'")
+print("he is\tawesome")
