94e62edd5111043562cdd47c1218c1f324952f38
Preeti Singh <115339754+ghpreeti@users.noreply.github.com>
2022-11-28 20:52:09 +0530

    Add files via upload

---
+++ ex8_Cipher.py
@@ -0,0 +1,18 @@
+# exercise one of the three 
+# sum of n number
+# ask the user for natural number "n"
+# print total from 1 to n
+
+n=int(input("enter the number: "))
+total=0
+i=1
+while i<=n:
+    total +=1
+    i+=1
+    print(total)
+
+
+
+
+
+
