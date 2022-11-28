aa6d6c60415af193150341210ca66f1ecfcbe18c
Preeti Singh <115339754+ghpreeti@users.noreply.github.com>
2022-11-28 20:34:09 +0530

    Add files via upload

---
+++ ex7_Cipher.py
@@ -0,0 +1,21 @@
+age=int(input("please enter your age: "))
+if 0<age<=3:
+    print("ticket price: free")
+if 3<age<=10:
+    print("ticket price: 150")
+if 10<age<=60:
+    print("ticket price: 250")
+else:
+    print("ticket price: 200")
+
+    
+
+
+
+
+
+
+
+
+
+
